8f69517ae31d4b2251c8385eba44ae61b988e9c3
afozbek <furkanozbek1995@gmail.com>
2019-03-28 22:38:42 +0300

    building component structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
   state = {
@@ -56,7 +56,7 @@ class App extends Component {
                   name={person.name}
                   age={person.age}
                   change={(e) => this.onNameChange(e, person.id)}
-                />
+                >{person.id}</Person>
               );
             })
           }
