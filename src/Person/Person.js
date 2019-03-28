7fe96e15d7727ba6d5988a6ff2fd291afc54b071
afozbek <furkanozbek1995@gmail.com>
2019-03-28 15:20:51 +0300

    small styling person

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import './Person.css';
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.change} />
+            <input type="text" onChange={props.change} value={props.name} />
         </div>
     )
 };
