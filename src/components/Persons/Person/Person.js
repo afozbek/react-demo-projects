339a6880ebb4efea92185d9eaff1e7c094eafa54
afozbek <furkanozbek1995@gmail.com>
2019-03-29 12:19:40 +0300

    logging queue of the lifecycle

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Person.css';
 const person = (props) => {
+    console.log('[Person.js] rendering...')
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import Person from './Person/Person';
-const persons = props => props.persons.map((person, index) => {
-    return (
-        <Person
-            click={() => props.clicked(index)}
-            name={person.name}
-            age={person.age}
-            change={(e) => props.changed(e, person.id)}
-            key={index}
-        >
-            {person.id}
-        </Person>
-    );
-});
+const persons = props => {
+    console.log('[Person.js] rendering..')
+    return props.persons.map((person, index) => {
+        return (
+            <Person
+                click={() => props.clicked(index)}
+                name={person.name}
+                age={person.age}
+                change={(e) => props.changed(e, person.id)}
+                key={index}
+            >
+                {person.id}
+            </Person>
+        );
+    });
+};
 
 export default persons;
