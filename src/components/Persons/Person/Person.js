522a4f3f9c0fdfbf850f5b3ceac96030f1e01f12
afozbek <furkanozbek1995@gmail.com>
2019-03-30 10:56:59 +0300

    rendering with props.children

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+
+import Auxiliary from '../../../hoc/Auxiliary';
+
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
         return (
-            <div className={classes.Person}>
+            <Auxiliary>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.change} value={this.props.name} />
-            </div>
+            </Auxiliary>
         );
     }
 
---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const auxiliary = props => props.children;
+
+export default auxiliary;
