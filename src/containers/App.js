e803f8fa87eb2b498989ff34f5fd8c3179255207
afozbek <furkanozbek1995@gmail.com>
2019-03-30 15:36:07 +0300

    better state update

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,8 @@ class App extends Component {
         { id: 'adadfrt', name: 'Yunus', age: 26 }
       ],
       showPersons: false,
-      showCockpit: true
+      showCockpit: true,
+      changeCounter: 0
     }
   }
   static getDerivedStateFromProps(props, state) {
@@ -53,10 +54,14 @@ class App extends Component {
     person.name = e.target.value;
     const persons = [...this.state.persons];
     persons[personIndex] = person;
-    this.setState({
-      persons: persons
-    })
-  }
+
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter++
+      }
+    });
+  };
 
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
