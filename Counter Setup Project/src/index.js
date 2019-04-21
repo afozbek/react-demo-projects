8e45c360a76d35e114941e31d24e67ec215d5cfb
afozbek <furkanozbek1995@gmail.com>
2019-04-21 22:23:50 +0300

    combining reducers to reducer

--- Counter Setup Project/src/index.js
+++ Counter Setup Project/src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import reducer from "./store/reducer";
+import counterReducer from "./store/reducers/counter";
+import storeReducer from "./store/reducers/result";
 
+const reducer = combineReducers({
+  ctr: counterReducer,
+  res: storeReducer
+});
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
   reducer /* preloadedState, */,
