cd0cb084ba45ecdffc9bb7ca0a199bf2f6ebe9e5
afozbek <furkanozbek1995@gmail.com>
2019-04-21 19:16:42 +0300

    updating redux-basics.js

--- Counter Setup Project/redux-basics.js
+++ Counter Setup Project/redux-basics.js
@@ -6,12 +6,29 @@ const initialState = {
 };
 // Reducer
 const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
   return state;
 };
 
 // Store
 const store = createStore(rootReducer);
 console.log(store.getState());
+
 // Dispatching
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+
+console.log(store.getState());
 
 //Subscription
