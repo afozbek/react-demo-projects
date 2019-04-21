d07a610b4c13beb7c8c358a091b02cc091ef5a21
afozbek <furkanozbek1995@gmail.com>
2019-04-21 21:08:25 +0300

    setup for redux extention in chrome

--- Counter Setup Project/src/index.js
+++ Counter Setup Project/src/index.js
@@ -8,7 +8,12 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import reducer from "./store/reducer";
 
-const store = createStore(reducer);
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducer /* preloadedState, */,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable */
 const app = (
   <Provider store={store}>
     <App />
