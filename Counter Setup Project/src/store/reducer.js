19cae78bceac0799698c1843aa8bbf02f04a7c15
afozbek <furkanozbek1995@gmail.com>
2019-04-21 19:49:48 +0300

    adding action.type INCREMENT

--- Counter Setup Project/src/store/reducer.js
+++ Counter Setup Project/src/store/reducer.js
@@ -2,7 +2,15 @@ const initialState = {
   counter: 0
 };
 const reducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
