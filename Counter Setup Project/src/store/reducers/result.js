c93afe28496a811a69d745164ef90c982d0930f5
afozbek <furkanozbek1995@gmail.com>
2019-04-21 22:23:23 +0300

    adding result Reducer

---
+++ Counter Setup Project/src/store/reducers/result.js
@@ -0,0 +1,29 @@
+import * as actions from "../actions";
+
+const initialState = {
+  results: []
+};
+const resultReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actions.STORE_RESULT:
+      return {
+        ...state,
+        results: [
+          ...state.results,
+          {
+            id: Math.random() * 1000,
+            value: action.ctr
+          }
+        ]
+      };
+    case actions.DELETE_RESULT:
+      return {
+        ...state,
+        results: state.results.filter(result => result.id !== action.id)
+      };
+    default:
+      return state;
+  }
+};
+
+export default resultReducer;
