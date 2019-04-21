4c7f86374bcc3d81ebee00c17af1a8b91eb6a9dc
afozbek <furkanozbek1995@gmail.com>
2019-04-21 16:27:37 +0300

    creating reducers

---
+++ Blog Project/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
+
+export default combineReducers({
+  posts: postsReducer,
+  users: usersReducer
+});
