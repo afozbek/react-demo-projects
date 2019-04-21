90881b457a0ebc5aa5c2ad4f644d97097de41114
afozbek <furkanozbek1995@gmail.com>
2019-04-21 22:23:36 +0300

    adding counter Reducer

---
+++ Counter Setup Project/src/store/reducers/counter.js
@@ -0,0 +1,34 @@
+import * as actions from "../actions";
+
+const initialState = {
+  counter: 0
+};
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actions.INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+
+    case actions.DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    case actions.ADD:
+      return {
+        ...state,
+        counter: state.counter + action.value
+      };
+    case actions.SUBSTRACT:
+      return {
+        ...state,
+        counter: state.counter - action.value
+      };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
