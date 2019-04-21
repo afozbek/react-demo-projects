59254d0c821339d40c74ba57e1058bdaed8f5bf0
afozbek <furkanozbek1995@gmail.com>
2019-04-21 19:20:11 +0300

    adding subscription

--- Counter Setup Project/redux-basics.js
+++ Counter Setup Project/redux-basics.js
@@ -25,10 +25,13 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+//Subscription
+store.subscribe(() => {
+  console.log("[Subscription]", store.getState());
+});
+
 // Dispatching
 store.dispatch({ type: "INC_COUNTER" });
 store.dispatch({ type: "ADD_COUNTER", value: 10 });
 
 console.log(store.getState());
-
-//Subscription
