0e9d2c6b6ac0ff40599939cc3e14b67ac2910754
afozbek <furkanozbek1995@gmail.com>
2019-05-11 16:24:29 +0300

    adding utitility function to result

--- Counter Setup Project/src/store/reducers/result.js
+++ Counter Setup Project/src/store/reducers/result.js
@@ -4,21 +4,28 @@ import { updateObject } from "../utility";
 const initialState = {
   results: []
 };
+
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(result => result.id !== action.id);
+  return updateObject(state, { results: updatedArray });
+};
+
+const storeResult = (state, action) => {
+  return updateObject(state, {
+    results: state.results.concat({
+      id: Math.random() * 1000,
+      value: action.ctr
+    })
+  });
+};
+
 const resultReducer = (state = initialState, action) => {
   switch (action.type) {
     case actions.STORE_RESULT:
-      return updateObject(state, {
-        results: state.results.concat({
-          id: Math.random() * 1000,
-          value: action.ctr
-        })
-      });
+      return storeResult(state, action);
 
     case actions.DELETE_RESULT:
-      const updatedArray = state.results.filter(
-        result => result.id !== action.id
-      );
-      return updateObject(state, { results: updatedArray });
+      return deleteResult(state, action);
 
     default:
       return state;
