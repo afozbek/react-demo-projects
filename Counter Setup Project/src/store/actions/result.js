6dc856cd7fdb998738ee05567465a34246b06c97
afozbek <furkanozbek1995@gmail.com>
2019-05-11 15:46:46 +0300

    adding Result action

---
+++ Counter Setup Project/src/store/actions/result.js
@@ -0,0 +1,22 @@
+import { STORE_RESULT, DELETE_RESULT } from "./actionsTypes";
+
+export const saveResult = ctr => {
+  return {
+    type: STORE_RESULT,
+    ctr: ctr
+  };
+};
+
+export const storeResult = ctr => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(saveResult(ctr));
+    }, 2000);
+  };
+};
+export const deleteResult = id => {
+  return {
+    type: DELETE_RESULT,
+    id: id
+  };
+};
