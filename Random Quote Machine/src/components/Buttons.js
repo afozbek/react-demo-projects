26d471259332b012dfd3c9f9fa3b736914c530f2
afozbek <furkanozbek1995@gmail.com>
2019-05-24 18:08:50 +0300

    adding onClick handler

--- Random Quote Machine/src/components/Buttons.js
+++ Random Quote Machine/src/components/Buttons.js
@@ -7,13 +7,14 @@ const buttons = props => {
       <a
         className="button"
         title="share_content"
+        rel="noopener noreferrer"
         target="_blank"
         href="https://twitter.com/intent/tweet"
         id="tweet-quote"
       >
         <i className="fab fa-twitter" />
       </a>
-      <button className="button" id="new-quote">
+      <button className="button" id="new-quote" onClick={props.clicked}>
         New Quote
       </button>
     </div>
