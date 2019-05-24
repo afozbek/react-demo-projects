30bde566e661491610364680b5f79d0f4917e470
afozbek <furkanozbek1995@gmail.com>
2019-05-24 18:08:09 +0300

    handling click event

--- Random Quote Machine/src/components/Quote.js
+++ Random Quote Machine/src/components/Quote.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import QuoteText from "./Quote_text";
 import QuoteAuthor from "./Quote_author";
@@ -6,12 +7,40 @@ import Buttons from "./Buttons";
 import "./Quote.css";
 
 class Quote extends Component {
+  state = {
+    text: "",
+    author: ""
+  };
+  getQuote = () => {
+    axios
+      .get(
+        "https://jsonplaceholder.typicode.com/posts/" +
+          Math.floor(Math.random() * 100)
+      )
+      .then(res => {
+        const { title, body } = res.data;
+        this.setState({
+          text: body,
+          author: title.slice(1, 10)
+        });
+      })
+      .catch(err => console.log(err));
+  };
+  componentDidMount() {
+    this.getQuote();
+  }
+
+  handleQuoteClick = () => {
+    console.log("Clickedd");
+    this.getQuote();
+  };
+
   render() {
     return (
       <div id="quote-box">
-        <QuoteText />
-        <QuoteAuthor />
-        <Buttons />
+        <QuoteText text={this.state.text} />
+        <QuoteAuthor author={this.state.author} />
+        <Buttons clicked={this.handleQuoteClick} />
       </div>
     );
   }
