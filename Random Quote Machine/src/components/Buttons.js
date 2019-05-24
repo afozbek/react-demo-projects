2c613406c32700eb200d2cffa8e27e30ff1d6423
afozbek <furkanozbek1995@gmail.com>
2019-05-24 15:39:39 +0300

    adding button component

---
+++ Random Quote Machine/src/components/Buttons.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Quote.css";
+
+const buttons = props => {
+  return (
+    <div className="buttons">
+      <a
+        className="button"
+        title="share_content"
+        target="_blank"
+        href="https://twitter.com/intent/tweet"
+        id="tweet-quote"
+      >
+        <i className="fab fa-twitter" />
+      </a>
+      <button className="button" id="new-quote">
+        New Quote
+      </button>
+    </div>
+  );
+};
+
+export default buttons;
