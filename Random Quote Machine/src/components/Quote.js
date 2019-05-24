5ff46c9b8129aa7456cb731f5fc650a2eba05052
afozbek <furkanozbek1995@gmail.com>
2019-05-24 18:38:12 +0300

    adding random color Handler

--- Random Quote Machine/src/components/Quote.js
+++ Random Quote Machine/src/components/Quote.js
@@ -11,6 +11,16 @@ class Quote extends Component {
     text: "",
     author: ""
   };
+
+  randomColor = () => {
+    let colorPatterns = "1234567890ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += colorPatterns[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
   getQuote = () => {
     axios
       .get(
@@ -19,6 +29,16 @@ class Quote extends Component {
       )
       .then(res => {
         const { title, body } = res.data;
+
+        const color = this.randomColor();
+
+        document.body.style.color = color;
+        document.body.style.backgroundColor = color;
+        document.getElementById("tweet-quote").style.color = "#fff";
+        document.getElementById("tweet-quote").style.backgroundColor = color;
+        document.getElementById("new-quote").style.color = "#fff";
+        document.getElementById("new-quote").style.backgroundColor = color;
+
         this.setState({
           text: body,
           author: title.slice(1, 10)
@@ -37,7 +57,7 @@ class Quote extends Component {
 
   render() {
     return (
-      <div id="quote-box">
+      <div id="quote-box" style={{ color: this.state.backgroundColor }}>
         <QuoteText text={this.state.text} />
         <QuoteAuthor author={this.state.author} />
         <Buttons clicked={this.handleQuoteClick} />
