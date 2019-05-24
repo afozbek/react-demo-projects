aab1caa7160d1a36008575928ae56d2ee21eebac
afozbek <furkanozbek1995@gmail.com>
2019-05-24 16:50:06 +0300

    adding footer

---
+++ Random Quote Machine/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "../index.css";
+
+const footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <span> by </span>
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://github.com/afozbek"
+      >
+        Abdullah Furkan Özbek
+      </a>
+    </div>
+  );
+};
+
+export default footer;
