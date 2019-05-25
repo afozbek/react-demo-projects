f8ae63d1d70de92687db9b1d88227529a4683aef
afozbek <furkanozbek1995@gmail.com>
2019-05-25 10:35:31 +0300

    updating quote_text

--- Random Quote Machine/src/components/Quote_text.js
+++ Random Quote Machine/src/components/Quote_text.js
@@ -5,8 +5,7 @@ const quote_text = props => {
   return (
     <div className="quote-text">
       <i className="fas fa-quote-left" />
-      <span id="text" dangerouslySetInnerHTML={{ __html: props.text }} />
-      {/* Life is what happens to you while you’re busy making other plans. */}
+      <span id="text" dangerouslySetInnerHTML={{ __html: props.quote }} />
     </div>
   );
 };
