1f682bda338a87417a1af24cb2923573946c55c4
afozbek <furkanozbek1995@gmail.com>
2019-06-02 11:21:24 +0300

    using context hook to manage auth

--- Todos Project - React Hooks/src/App.js
+++ Todos Project - React Hooks/src/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Todo from "./components/Todo";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import AuthContext from "./auth-context";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Todo />
-      </div>
-    );
-  }
-}
+const app = props => {
+  const [page, setPage] = useState("auth");
+  const [authStatus, setAuthStatus] = useState(false);
 
-export default App;
+  const switchPage = pageName => {
+    setPage(pageName);
+  };
+
+  const login = () => {
+    setAuthStatus(true);
+  };
+
+  return (
+    <div>
+      <AuthContext.Provider value={{ status: authStatus, login: login }}>
+        <Header
+          onLoadTodos={switchPage.bind(this, "todos")}
+          onLoadAuth={switchPage.bind(this, "auth")}
+        />
+        <hr />
+        {page === "auth" ? <Auth /> : <Todo />}
+      </AuthContext.Provider>
+    </div>
+  );
+};
+
+export default app;
